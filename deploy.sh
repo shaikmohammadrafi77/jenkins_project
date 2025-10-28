c89ca3916bb7b716565b34939d25e65e6b127c8a
shaikmohammadrafi77 <MOHAMMADRAFI6633@GMAIL.COM>
2025-10-28 06:50:31 -0700

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+cd /home/ec2-user/CustomE-BugTracker
+git pull origin main
+
+python3 -m venv venv
+. venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+python3 app.py || true
+nohup python3 app.py >app.log 2>&1 &
+echo "deployment succesfully!"
